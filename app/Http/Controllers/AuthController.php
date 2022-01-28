57d2bd90aa89bdc2536ebf62093c1f3bd259a70f
aliluay992 <luaya577@gmail.com>
2022-01-28 11:50:35 +0300

    ali

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -209,7 +209,6 @@ class AuthController extends Controller
                     'min:8',
                     'same:confirm_password',             // must be at least 10 characters in length
                     'regex:/[a-z]/',      // must contain at least one lowercase letter
-                    'regex:/[A-Z]/',      // must contain at least one uppercase letter
                     'regex:/[0-9]/',      // must contain at least one digit
                     // 'regex:/[@$!%*#?&]/', // must contain a special character
                 ],
