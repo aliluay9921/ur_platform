01e48d107013e97a205074f38dfa772cd2ab0df0
aliluay992 <luaya577@gmail.com>
2022-01-31 18:45:03 +0300

    ali

--- routes/channels.php
+++ routes/channels.php
@@ -36,6 +36,7 @@ Broadcast::channel("transaction_socket.{user_id}", function ($user, $user_id) {
 });
 
 Broadcast::channel("comment_socket.{ticket_id}", function ($ticket_id, $user) {
+    error_log("" . $user->user_type);
     if ($user->user_type == 1 && $user->user_type == 2) {
         $ticket = Ticket::find($ticket_id);
     } else {
