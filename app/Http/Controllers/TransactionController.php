acbe3ddfe719c1274045c5f19166477d5e78dcea
aliluay992 <luaya577@gmail.com>
2022-02-08 14:27:55 +0300

    ali luay

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -211,8 +211,7 @@ class TransactionController extends Controller
                                 'last_order' => $order->id
                             ]);
                             $notify =  Notifications::create([
-                                'title' => trans('message.received.points.to.user.title'),
-                                "body" => trans("message.received.points.to.user") . $transactions_points->user_id,
+                                'title' => trans("message.received.points.to.user") . $transactions_points->user_id,
                                 "target_id" => $order->id,
                                 "to_user" =>  $to_user->id,
                                 "from_user" => auth()->user()->id,
