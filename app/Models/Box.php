c96cee6f35b7699beb550b922fb998308a38be5b
aliluay992 <luaya577@gmail.com>
2022-02-09 13:18:54 +0300

    ali luay

--- app/Models/Box.php
+++ app/Models/Box.php
@@ -11,4 +11,22 @@ class Box extends Model
 {
     use HasFactory, Uuids;
     protected $guarded = [];
+    // protected $casts = [""];
+
+    public function getTotalValueAttribute($value)
+    {
+        return ceil($value);
+    }
+    public function getCompanyRatioAttribute($value)
+    {
+        return ceil($value);
+    }
+    public function getProgrammerRatioAttribute($value)
+    {
+        return ceil($value);
+    }
+    public function getManagmentRatioAttribute($value)
+    {
+        return ceil($value);
+    }
 }
