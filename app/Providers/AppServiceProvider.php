f9f0ae36c8d1aa3794b958b4e6bdea8c392b90cf
aliluay992 <luaya577@gmail.com>
2022-02-16 14:43:46 +0300

    ali luay

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->environment('online')) {
-            \URL::forceScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
